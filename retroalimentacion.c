0aa23a28029467294f75ef0cb0011aba788d8646
Eduardo Sanchez <eduardo.sanchez4559@alumnos.udg.mx>
2020-09-16 01:06:49 -0500

    primer parte terminada suma numeros e imprime promedio

--- retroalimentacion.c
+++ retroalimentacion.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 
+void capturar_enteros(){
+	int enteros[5], i;
+	float promedio;
+	int suma = 0;
+	for(i=0;i<5;i++)
+	{
+		scanf("%i",&enteros[i]);
+	}
+	for(i=0;i<5;i++)
+	{
+		printf("%i\n", enteros[i]);
+	}
+	for(i=0;i<5;i++)
+	{
+		suma = suma + enteros[i]; // suma+=enteros[i]
+	}
+	printf("Suma: %i\n", suma);
+	printf("Promedio: %f\n", suma/5.0);
+}
 int main(){
 	char op;
 	
@@ -10,6 +29,20 @@ int main(){
 		printf("3) Agregar personaje\n");
 		printf("0) salir\n");
 		scanf("%c", &op);
+		fflush(stdin);
+		 
+		switch(op)
+		{
+			case '1':
+				capturar_enteros();
+				break;
+			case '2':
+				break;
+			case '3':
+				break;
+			default:
+				break;
+		}
 	}
 	while(op != '0');
 	
